f29e38516483ad6e34c1f33d3ebfd78c59866587
TUHINDAS9670 <drup9672@gmail.com>
2025-10-15 11:54:50 +0530

    Optimized Footer component's code

---
+++ src/utils/FooterData.js
@@ -0,0 +1,29 @@
+const footerLinks = [
+  {
+    linkHeading: "Products",
+    linkItems: ["Exchange", "Wallet", "API", "Institutional", "DeFi Platform"],
+  },
+  {
+    linkHeading: "Resources",
+    linkItems: [
+      "Blog",
+      "Tutorials",
+      "Market Data",
+      "Documentation",
+      "Help Center",
+    ],
+  },
+  {
+    linkHeading: "Company",
+    linkItems: ["About", "Careers", "Press", "Legal & Privacy", "Contact Us"],
+  },
+];
+const socialLinks = [
+  { icon: "facebook", url: "#" },
+  { icon: "twitter", url: "#" },
+  { icon: "instagram", url: "#" },
+  { icon: "linkedin", url: "#" },
+  { icon: "github", url: "#" },
+];
+
+export {footerLinks,socialLinks}
